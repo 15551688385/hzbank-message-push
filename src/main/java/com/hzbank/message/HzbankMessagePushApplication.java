665ba67bd1dd4b13e38b1e31267eec16d846ecaf
15551688385 <1518078352@qq.com>
2022-07-21 15:14:20 +0800

    1

---
+++ src/main/java/com/hzbank/message/HzbankMessagePushApplication.java
@@ -0,0 +1,13 @@
+package com.hzbank.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzbankMessagePushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzbankMessagePushApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hzbank/message/HzbankMessagePushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzbank.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HzbankMessagePushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
